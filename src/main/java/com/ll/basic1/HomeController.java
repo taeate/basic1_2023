cb795f4f248b6c34a99715d0f72de2b98c275dd8
taeate <yes3144@nate.com>
2023-03-14 11:44:22 +0900

    home/increse 요청시 숫자 ì매번 증가

--- src/main/java/com/ll/basic1/HomeController.java
+++ src/main/java/com/ll/basic1/HomeController.java
@@ -6,19 +6,17 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
+    private int count = -1;
+
     @GetMapping("/home/main")
     @ResponseBody
     public String showMain(){
         return "안녕하세요";
     }
-    @GetMapping("/home/main2")
+    @GetMapping("/home/increase")
     @ResponseBody
-    public String showHello(){
-        return "반갑습니다.";
-    }
-    @GetMapping("/home/main3")
-    @ResponseBody
-    public String showJoy(){
-        return "즐거웠습니다";
+    public int showIncrease(){
+        count++;
+        return count;
     }
 }
