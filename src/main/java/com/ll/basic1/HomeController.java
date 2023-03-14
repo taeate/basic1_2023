3b11352477d9c99dfc48fcbcc85ff531d2828d91
taeate <yes3144@nate.com>
2023-03-14 11:55:24 +0900

    ìQueryString사용 url 에 파라미터를 받아서 응답받기

--- src/main/java/com/ll/basic1/HomeController.java
+++ src/main/java/com/ll/basic1/HomeController.java
@@ -2,6 +2,7 @@ package com.ll.basic1;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -18,5 +19,10 @@ public class HomeController {
     public int showIncrease(){
         count++;
         return count;
+    }
+    @GetMapping("/home/plus")
+    @ResponseBody
+    public int showPlus(@RequestParam(defaultValue = "0") int a , @RequestParam int b){
+        return a + b;
     }
 }
