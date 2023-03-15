c79f09d9457cb497c4385dd44ae61a36ff6c5381
taeate <yes3144@nate.com>
2023-03-15 14:01:32 +0900

    MemberRepository 도입

--- src/main/java/com/ll/basic1/boundedContext/member/controller/MemberController.java
+++ src/main/java/com/ll/basic1/boundedContext/member/controller/MemberController.java
@@ -4,34 +4,15 @@ import com.ll.basic1.base.rsData.RsData;
 import com.ll.basic1.boundedContext.member.service.MemberService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.HashMap;
-import java.util.Map;
 
 
 @Controller
 public class MemberController {
-    //  V1//http://localhost:8080/member/login?username=user1&password=1234
-//    @GetMapping("/member/login")
-//    @ResponseBody
-//    public Map<String, Object> Login(@RequestParam String username, @RequestParam String password) {
-//
-//        Map<String, Object> result = new HashMap<>();
-//
-//        if("user1".equals(username) && "1234".equals(password)){
-//            result.put("resultCode", "S-1");
-//            result.put("msg", username + "님 환영합니다");
-//        } else {
-//            result.put("resultCode", "F-1");
-//            result.put("msg", "비밀번호가 일치하지않습니다");
-//        }
-//        return result;
-//    }
     private final MemberService memberService;
 
-    public MemberController(){
+    public  MemberController(){
         memberService = new MemberService();
     }
     @GetMapping("/member/login")
